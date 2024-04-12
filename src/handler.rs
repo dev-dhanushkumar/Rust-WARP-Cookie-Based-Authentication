b8c1883e77d95cf7a95fadfbdfd288a4d6718b0d
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-04-12 09:55:10 +0300

    logout and get_me updated

--- src/handler.rs
+++ src/handler.rs
@@ -4,17 +4,13 @@ use crate::{
     model::{LoginUserSchema, RegisterUserSchema, TokenClaims, User},
     response::FilteredUser,
 };
-// use cookie::Cookie;
-use time::Duration as Dur;
-
 
 use argon2::{
     password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
     Argon2,
 };
 use chrono::{Duration, Utc};
-use cookie::Cookie;
-use jsonwebtoken::{encode, jwk, EncodingKey, Header};
+use jsonwebtoken::{encode, EncodingKey, Header};
 use serde_json::json;
 use sqlx::{Pool, Postgres, Row};
 use uuid::Uuid;
@@ -150,6 +146,37 @@ async fn login_user_handler(
     return Ok(response_with_cookie);
 }
 
+async fn logout_handler(id:Uuid) -> Result<impl Reply, warp::Rejection> {
+    println!("User id: {}", id);
+    let cookie_str = format!("token=; Max-Age=-1; httponly; path=/");
+    let json_response = reply::json(&json!({
+        "status": "success"
+    }));
+
+    let response_with_cookie = warp::reply::with_header(
+        json_response,
+        SET_COOKIE,
+        HeaderValue::from_str(&cookie_str).unwrap(),
+    );
+    return Ok(response_with_cookie);
+}
+
+
+async fn get_me_handler(user_id:Uuid, pool: Pool<Postgres>) -> Result<impl Reply, Rejection> {
+    let user = sqlx::query_as!(User, "SELECT * FROM users WHERE id = $1", user_id)
+        .fetch_one(&pool)
+        .await
+        .unwrap();
+
+    let json_response = json!({
+        "status": "success",
+        "data": json!({
+            "user": filter_user_record(&user)
+        })
+    });
+
+    Ok(warp::reply::json(&json_response))
+}
 
 
 pub fn routes(
@@ -173,10 +200,27 @@ pub fn routes(
         .and(with_pool(pool.clone()))
         .and_then(login_user_handler);
 
+    let logout_handler = warp::path("api")
+        .and(warp::path("auth"))
+        .and(warp::path("logout"))
+        .and(warp::get())
+        .and(jwt_auth::auth_validation(config.clone()))
+        .and_then(logout_handler);
+
+    let get_me_handler = warp::path("api")
+        .and(warp::path("users"))
+        .and(warp::path("me"))
+        .and(warp::get())
+        .and(jwt_auth::auth_validation(config.clone()))
+        .and(with_pool(pool.clone()))
+        .and_then(get_me_handler);
+
 
 
     register_user_handler
         .or(login_user_handler)
+        .or(get_me_handler)
+        .or(logout_handler)
 
 }
 
